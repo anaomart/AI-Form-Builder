89f2bf4186e080703a6800ba2090413aae58bf84
anaomart <anaomart@gmail.com>
2024-12-19 18:58:15 +0200

    fix devlopment next 15 new pramas error

--- app/submit/[formUrl]/page.tsx
+++ app/submit/[formUrl]/page.tsx
@@ -2,13 +2,11 @@ import { GetFormContentByUrl } from '@/actions/forms';
 import { FormElementInstance } from '@/components/ui-components/FormElements';
 import FormElementSubmitComponent from '@/components/ui-components/FormElementSubmitComponent';
 import React from 'react'
-type PageProps = {
-  params: {
-    formUrl: string;
-  };
-};
-export default async function SubmitPage({params}:PageProps) {
 
+type Params =  Promise<{ formUrl: string }>
+
+export default async function SubmitPage(props:{params:Params}) {
+  const params = await props.params ;
   const form = await GetFormContentByUrl(params.formUrl)
   if(!form) return null 
 
